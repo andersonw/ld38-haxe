f29e8011ef59be5f6e1e9c790854d07ae3491647
Anderson Wang <anderson.wang1994@gmail.com>
2017-04-23 17:32:27 -0400

    add coin intro level

--- source/Registry.hx
+++ source/Registry.hx
@@ -5,6 +5,7 @@ class Registry
     public static var levelList:Array<String> = [AssetPaths.trivial_level__tmx,
                                                  AssetPaths.scale_intro__tmx,
                                                  AssetPaths.best_level__tmx,
+                                                 AssetPaths.coin_intro__tmx,
                                                  AssetPaths.two_coins__tmx,
                                                  AssetPaths.three_balls__tmx,
                                                  AssetPaths.gates_intro__tmx,
@@ -12,6 +13,6 @@ class Registry
                                                  AssetPaths.unscalable_ball__tmx,
                                                  AssetPaths.target_practice__tmx,
                                                  AssetPaths.scale_ferry__tmx];
-                                                 
+
     public static var currLevel:Int = 0; 
 }
--- source/Wall.hx
+++ source/Wall.hx
@@ -7,7 +7,7 @@ class Wall extends ScalableSprite
     public function new(?X:Float=0, ?Y:Float=0, ?width:Int=32, ?height:Int=32)
     {
         super(X, Y);
-        makeGraphic(width, height, FlxColor.BLUE);
+        makeGraphic(width, height, FlxColor.GRAY);
         immovable=true;
     }
 }
