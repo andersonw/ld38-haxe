d86b1a9ed159a974afaf729cbd52b1673d232b68
Anderson Wang <anderson.wang1994@gmail.com>
2017-04-23 17:17:10 -0400

    add trivial level, rename everything

--- source/Registry.hx
+++ source/Registry.hx
@@ -2,16 +2,16 @@ package;
 
 class Registry
 {
-    public static var levelList:Array<String> = [AssetPaths.scale_ferry__tmx,
-                                                 AssetPaths.interesting_level2__tmx,
+    public static var levelList:Array<String> = [AssetPaths.trivial_level__tmx,
+                                                 AssetPaths.scale_intro__tmx,
+                                                 AssetPaths.best_level__tmx,
+                                                 AssetPaths.two_coins__tmx,
+                                                 AssetPaths.three_balls__tmx,
+                                                 AssetPaths.gates_intro__tmx,
+                                                 AssetPaths.coin_square__tmx,
+                                                 AssetPaths.unscalable_ball__tmx,
                                                  AssetPaths.target_practice__tmx,
-                                                 AssetPaths.interesting_level__tmx,
-                                                 AssetPaths.test_gates__tmx,
-                                                 AssetPaths.test2__tmx,
-                                                 AssetPaths.colin_test__tmx,
-                                                 AssetPaths.level3__tmx,
-                                                 AssetPaths.level1__tmx,
-                                                 AssetPaths.test_level__tmx, 
-                                                 AssetPaths.level2__tmx];
+                                                 AssetPaths.scale_ferry__tmx];
+                                                 
     public static var currLevel:Int = 0; 
 }
