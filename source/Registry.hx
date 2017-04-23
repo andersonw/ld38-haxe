b09473e66f5885e6eeaee9015d9fa4f985bb3aea
Jon Schneider <jschneider2013@gmail.com>
2017-04-22 23:52:13 -0400

    added level2 to registry

--- source/Registry.hx
+++ source/Registry.hx
@@ -2,6 +2,8 @@ package;
 
 class Registry
 {
-    public static var levelList:Array<String> = [AssetPaths.test_level__tmx, AssetPaths.level1__tmx];
+    public static var levelList:Array<String> = [AssetPaths.test_level__tmx, 
+                                                 AssetPaths.level1__tmx,
+                                                 AssetPaths.level2__tmx];
     public static var currLevel:Int = 0; 
 }
