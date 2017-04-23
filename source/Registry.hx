5896e5d0d46b9e58424a5df08171b89d944d9064
clue <colin.h.lu@gmail.com>
2017-04-23 13:31:23 -0500

    new levels

--- source/Registry.hx
+++ source/Registry.hx
@@ -2,7 +2,9 @@ package;
 
 class Registry
 {
-    public static var levelList:Array<String> = [AssetPaths.level3__tmx,
+    public static var levelList:Array<String> = [AssetPaths.test2__tmx,
+                                                 AssetPaths.colin_test__tmx,
+                                                 AssetPaths.level3__tmx,
                                                  AssetPaths.level1__tmx,
                                                  AssetPaths.test_level__tmx, 
                                                  AssetPaths.level2__tmx];
